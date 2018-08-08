633335eb06f27f4cb971a6ed2a2872a1429842d0
Lawynn Jana <jlawynn@gmail.com>
2018-08-08 18:08:37 -0400

    add response for successful local login

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -9,6 +9,13 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+
+    // user's email and password are auth'd
+    // give them a token
+    res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next){
   const email = req.body.email;
   const password = req.body.password;
